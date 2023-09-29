c556d4649f7da522174d7fca48c25066f1b0f0c3
LAPTOP-9IINQK99\souki <g20072@e.hiroshima-cu.ac.jp>
2023-09-29 22:49:36 +0900

    パスワード確認のコード

--- omikuji.js
+++ omikuji.js
@@ -1,2 +1,12 @@
 console.log("実行開始")
 console.log("実行終了")
+var password = 1000
+window.onload = function judge() {
+    var input_pass = prompt("パスワード");
+    //入力された値とパスワードの確認
+    if (input_pass != password){
+        //エラー表示
+      alert("パスワードが違います")
+      judge()
+    }
+}
